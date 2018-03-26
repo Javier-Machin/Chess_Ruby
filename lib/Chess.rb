d0324c31b23c0f61b663e4c4a13939ddd8bc3967
Javier Machin <javier_machin_acosta@hotmail.com>
2018-03-26 17:55:23 +0100

    Fix pieces starting positions

--- lib/Chess.rb
+++ lib/Chess.rb
@@ -33,45 +33,39 @@ class Chess
   def create_board
   	@board["a8"] = Rook.new("Blacks")
     @board["b8"] = Knight.new("Blacks")
-    @board["g8"] = Knight.new("Blacks")
     @board["c8"] = Bishop.new("Blacks")
-    @board["f8"] = Bishop.new("Blacks")
     @board["d8"] = Queen.new("Blacks")
     @board["e8"] = King.new("Blacks")
+    @board["f8"] = Bishop.new("Blacks")
+    @board["g8"] = Knight.new("Blacks")
     @board["h8"] = Rook.new("Blacks") 
   	8.times { |index| @board["#{(index + 97).chr}7"] = BlackPawn.new("Blacks") }
-    8.times { |index| @board["#{(index + 97).chr}3"] = nil }
-    8.times { |index| @board["#{(index + 97).chr}4"] = nil }
-    8.times { |index| @board["#{(index + 97).chr}5"] = nil }
     8.times { |index| @board["#{(index + 97).chr}6"] = nil }
+    8.times { |index| @board["#{(index + 97).chr}5"] = nil }
+    8.times { |index| @board["#{(index + 97).chr}4"] = nil }
+    8.times { |index| @board["#{(index + 97).chr}3"] = nil }
     8.times { |index| @board["#{(index + 97).chr}2"] = WhitePawn.new("Whites") }
     @board["a1"] = Rook.new("Whites") 
     @board["b1"] = Knight.new("Whites")
-    @board["g1"] = Knight.new("Whites")
     @board["c1"] = Bishop.new("Whites")
-    @board["f1"] = Bishop.new("Whites")
     @board["d1"] = Queen.new("Whites")
     @board["e1"] = King.new("Whites")
+    @board["f1"] = Bishop.new("Whites")
+    @board["g1"] = Knight.new("Whites")
     @board["h1"] = Rook.new("Whites")
   end
 
   def print_board
-
     @board.each do |key, value|
-      if key[0] != "h"
-        if value == nil
-          print "[  ]"
-        else
-          print "[#{value.symbol} ]"
-        end
+      if key[0] == "h"
+        value == nil ? (puts "[  ]") : (puts "[#{value.symbol} ]")
+      elsif key[0] == "a"
+        value == nil ? (print "#{key[1]} [  ]") : (print "#{key[1]} [#{value.symbol} ]")
       else
-        if value == nil
-          puts "[  ]"
-        else
-          puts "[#{value.symbol} ]"
-        end
+        value == nil ? (print "[  ]") : (print "[#{value.symbol} ]") 
       end
     end
+    puts "   a   b   c   d   e   f   g   h"
   end
 
   def check_input(input, type)
@@ -126,35 +120,40 @@ class Chess
   class Rook < Piece
     def initialize(team)
       super
-      team == "Whites" ? @symbol = "\u265C".encode("utf-8") : @symbol = "\u2656".encode("utf-8")
+      team == "Whites" ? @symbol = "\u265C".encode("utf-8") :
+                         @symbol = "\u2656".encode("utf-8")
     end
   end
 
   class Knight < Piece
     def initialize(team)
       super
-      team == "Whites" ? @symbol = "\u265E".encode("utf-8") : @symbol = "\u2658".encode("utf-8")
+      team == "Whites" ? @symbol = "\u265E".encode("utf-8") : 
+                         @symbol = "\u2658".encode("utf-8")
     end
   end
 
   class Bishop < Piece
     def initialize(team)
       super
-      team == "Whites" ? @symbol = "\u265D".encode("utf-8") : @symbol = "\u2657".encode("utf-8")
+      team == "Whites" ? @symbol = "\u265D".encode("utf-8") :
+                         @symbol = "\u2657".encode("utf-8")
     end
   end
 
   class Queen < Piece
     def initialize(team)
       super
-      team == "Whites" ? @symbol = "\u265B".encode("utf-8") : @symbol = "\u2655".encode("utf-8")
+      team == "Whites" ? @symbol = "\u265B".encode("utf-8") :
+                         @symbol = "\u2655".encode("utf-8")
     end
   end
 
   class King < Piece
     def initialize(team)
       super
-      team == "Whites" ? @symbol = "\u265A".encode("utf-8") : @symbol = "\u2654".encode("utf-8")
+      team == "Whites" ? @symbol = "\u265A".encode("utf-8") :
+                         @symbol = "\u2654".encode("utf-8")
     end
   end
 
